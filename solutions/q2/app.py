d50472eeb9c34479d3df58e0c39a6f9b96964712
philscalo <phillip.scalo@gmail.com>
2021-05-31 10:10:55 -0700

    modifications to question2

---
+++ solutions/q2/app.py
@@ -0,0 +1,9 @@
+def factorize(num):
+    """
+    given an integer, return the factorial
+    """
+    return num
+
+
+if __name__ == '__main__':
+    factorize(input())
---
+++ solutions/q2/test_app.py
@@ -0,0 +1,11 @@
+from q2.app import factorize
+import unittest
+
+class TestApp(unittest.TestCase):
+
+    def test_app(self):
+        self.assertEqual(factorize(8), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
