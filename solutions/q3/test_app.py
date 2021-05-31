5a75994b3b3c949debba6ca42adf76109e6f1f1f
philscalo <phillip.scalo@gmail.com>
2021-05-31 16:00:37 -0700

    added q3 test

---
+++ solutions/q3/test_app.py
@@ -0,0 +1,14 @@
+import unittest
+
+from solutions.q3.app import square_dict
+
+
+class TestApp(unittest.TestCase):
+
+    def test_square_dict(self):
+        self.assertEqual(square_dict(3), {1: 1, 2: 4, 3: 9})
+
+
+
+if __name__ == '__main__':
+    unittest.main()
