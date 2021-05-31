af5a3ea8d0ce9e423f3b5a5e25efbed2d5021367
philscalo <phillip.scalo@gmail.com>
2021-05-31 14:52:43 -0700

    added tests and code

--- solutions/q2/app.py
+++ solutions/q2/app.py
@@ -2,7 +2,14 @@ def factorize(num):
     """
     given an integer, return the factorial
     """
-    return num
+    processed = 0
+    running_total =1 
+    current = num
+    for x in range(1,num+1):
+        running_total *= current
+        current -= 1
+    processed = running_total
+    return processed
 
 
 if __name__ == '__main__':
--- solutions/q2/test_app.py
+++ solutions/q2/test_app.py
@@ -6,7 +6,7 @@ from solutions.q2.app import factorize
 class TestApp(unittest.TestCase):
 
     def test_app(self):
-        self.assertEqual(factorize(8), 8)
+        self.assertEqual(factorize(8), 40320)
 
 
 if __name__ == '__main__':
