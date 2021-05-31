3a7db669cb0d0352abc272e6f85586fa3edd81e6
philscalo <phillip.scalo@gmail.com>
2021-05-31 09:36:50 -0700

    modify response to q1

--- solutions/q1/q1.py
+++ solutions/q1/q1.py
@@ -1,3 +1,17 @@
-listx = range(2000, 3201, 7)
+def sevn_not_five(lowr, uppr):
+    """
+    Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+    between 2000 and 3200 (both included).
 
-[print(number) for number in listx]
+    The numbers obtained should be printed in a comma-separated sequence on a single line.
+"""
+
+    listx = [*range(lowr, uppr + 1, 7)]
+    listy = list(filter(lambda number: number % 5 != 0, listx))
+    return_str = ", ".join(str(num) for num in listy)
+    print(return_str)
+    return return_str
+#[print(','.join(number)) for number in listx if number % 5 != 0]
+
+if __name__ == '__main__':
+    sevn_not_five(2000, 3200)
