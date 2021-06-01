fac97d3582f71ad4ce4ba0f5a3dde0900369edc7
philscalo <phillip.scalo@gmail.com>
2021-05-31 17:11:17 -0700

    added code to app

---
+++ solutions/q3/app.py
@@ -0,0 +1,7 @@
+def square_dict(num):
+    results = dict(zip([x for x in range(1, num + 1)], [x*x for x in range(1, num + 1)]))
+    return results
+
+
+if __name__ == '__main__':
+    square_dict(input())
