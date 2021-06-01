6187fb9afbc9d12fe0cbacd40972f9007f22d6e5
philscalo <phillip.scalo@gmail.com>
2021-05-31 17:20:38 -0700

    challenge solved

--- solutions/q3/app.py
+++ solutions/q3/app.py
@@ -1,5 +1,5 @@
 def square_dict(num):
-    results = dict(zip([x for x in range(1, num + 1)], [x*x for x in range(1, num + 1)]))
+    results = dict(zip([x for x in range(1, int(num) + 1)], [x*x for x in range(1, int(num) + 1)]))
     return results
 
 
